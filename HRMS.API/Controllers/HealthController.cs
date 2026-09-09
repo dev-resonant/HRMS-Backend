9525543f5ab6b0802d7cd90949637f5b53639b0e
adity-4774 <>
2026-09-09 18:41:11 +0530

    Add health endpoint and remove test controllers

---
+++ HRMS.API/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.API.Controllers
+{
+    [ApiController]
+    [Route("healthz")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "healthy"
+            });
+        }
+    }
+}
